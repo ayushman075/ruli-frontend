63ff27519add2a88a63fb11fce2f9311dbd10025
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2024-10-24 16:43:21 +0530

    Domain changed

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/',
+  baseURL: 'https://rule-backend.onrender.com/api/v1/',
   timeout: 5000, 
   headers: {
     'Content-Type': 'application/json',
