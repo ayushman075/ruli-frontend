df849451bd7e7abf79af1f2191d2b17060a00f08
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2024-10-26 12:03:43 +0530

    Timeout changed

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // Create an instance of axios
 const axiosInstance = axios.create({
   baseURL: 'https://rule-backend.onrender.com/api/v1/',
-  timeout: 5000, 
+  timeout: 500000, 
   headers: {
     'Content-Type': 'application/json',
   },
